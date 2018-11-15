e3da35527c990238dfce8e5f0ae444548de61a74
aidam38 <krivka.adam@gmail.com>
2018-11-15 07:32:03 +0100

    Minor additions

--- adamos.sh
+++ adamos.sh
@@ -2,14 +2,29 @@
 # Automatic instalation script of adamos - Void linux based operation system with dwm, st, ranger ... for personal use of Adam Krivka
 # IMPORTANT: Make sure you run this script as the default user (you) and as 'sudo'
 
+# Clone dotfiles from github
+cd /home/adam
+git clone --depth 1 https://github.com/aidam38/adamos_void /home/adam &>/dev/null
+
 # Disabling services for obsolete ttys
 cd /var/service
-rm -f agetty-tty3 & rm -f agetty-tty4 & rm -f agetty-tty5 & rm -f agetty-tty6 
+rm -f agetty-tty3 & rm -f agetty-tty4 & rm -f agetty-tty5 & rm -f agetty-tty6 &>/dev/null
+
+# Disabling grub login menu and setting up automatic login
+echo "Disabling grub os prober and setting up automatic login"
+cp -R /home/adam/scripts/install/agetty-autologin-tty1 /etc/sv/ &>/dev/null
+ln -s /etc/sv/agetty-autologin-tty1 /var/service 
+rm /var/service/agetty-tty1 &>/dev/null
+
+cp -f /home/adam/scripts/install/grub /etc/default/grub
+update-grub &>/dev/null
 
 # Synchronizing the package system and installing basic programs
-xbps-install -Suv
-xbps-install -Sy nvim ranger qutebrowser feh
-xbps-install -Sy git make xorg-minimal xorg-fonts xf86-video-intel base-devel libX11-devel libXft-devel libXinerama-devel
+xbps-install -Suv &>/dev/null
+while IFS=, read -r program; do
+	echo "(xbps) Installing " $program
+	xbps-install -Sy $program &>/dev/null
+done;
 
 # Installing dwm, st and dmenu from my git repositories (some programs are my own forks)
 cd /home/adam
@@ -36,8 +51,3 @@ cd dmenu
 sudo make clean install
 
 cd /home/adam
-
-# Disabling grub login menu and setting up automatic login
-cp -R /home/adam/scripts/install/agetty-autologin-tty1 /etc/sv/
-ln -s /etc/sv/agetty-autologin-tty1 /var/service
-rm /var/service/agetty-tty1
